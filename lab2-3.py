c9de345d99075e2646047134619b6f873653104c
naz <p24nthompson@fairfieldprep.org>
2023-09-21 12:32:12 -0400

    lab submit

---
+++ lab2-3.py
@@ -0,0 +1,14 @@
+# Create a Python file named lab_2-3.py
+
+# Set a equal to 2. Using the type() function, print the type of variable a
+a = 2
+print (a)
+# Change the type of a to a float using a = float(a). What is the result of printing type(a) now?
+a = float(a)
+print (a)
+# Convert a to a string. Check its type with a print statement. What has changed?
+a = str(a)
+print (a)
+# Convert a to a Boolean. Print the result of a == 2. What is the result? Why? (Answer as a comment)
+a == 2.
+print (a)
